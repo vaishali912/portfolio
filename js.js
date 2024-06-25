40fe23b2000508ba7f62ba549eb80b978cad6bc3
vaishali912 <kunwarvaishali@gmail.com>
2024-06-25 10:23:17 +0530

    Iinitial commit

--- js.js
+++ js.js
@@ -1,46 +1,10 @@
-var typed = new Typed(".text",{
-    strings:["WEB-DEVELOPER"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
-
-let right = document.getElementsByClassName("right")[0];
-let toogle = document.getElementsByClassName("toogle")[0];
-let close = document.getElementsByClassName("close")[0];
-if (toogle) {
-    toogle.addEventListener("click", () => {
-        right.classList.add("show-right-un");
-    });
-}
-if(close){
-    close.addEventListener("click",()=>{
-        right.classList.remove("show-right-un");
-    })
-}
-const list = document.querySelectorAll("right-list");
-
-function action(){
-    let nav = document.getElementsByClassName("right")[0];
-    nav.classList.remove("show-right-un"); 
-}
-list.forEach(n => n.addEventListener("click",action));
-
-function color(){
-    let nav = document.getElementsByClassName("nav")[0];
-    if(this.ScrollY>=80) nav.classList.add("scroll-nav"); else nav.classList.remove("scroll-nav")
-}
-window.addEventListener("scroll",color);
 
 const theme = document.querySelector("#theme");
 const modal = document.querySelector(".customise-theme");
 const font = document.querySelectorAll(".chose-size span");
 const palette = document.querySelectorAll(".chose-color span");
 var root = document.querySelector(":root");
-const bg1 = document.querySelector(".bg-1");
-const bg2 = document.querySelector(".bg-2");
-const bg3 = document.querySelector(".bg-3");
+
 const openmodal = () =>{
   
     modal.style.display = "grid";
@@ -111,65 +75,80 @@ palette.forEach(color =>{
 
 
 
-let light;
-let white;
-let dark;
-
-const changebg = ()=>{
-    root.style.setProperty("--light-color-lightness--",light);
-    root.style.setProperty("--white-color-lightness--",white);
-    root.style.setProperty("--dark-color-lightness--",dark);
-}
-bg1.addEventListener("click",()=>{
-   
-    bg1.classList.add("active");
-bg2.classList.remove("active");
-bg3.classList.remove("active");
-
-window.location.reload();
-})
-bg2.addEventListener("click",()=>{
-    dark = "95%";
-    white = "20%";
-    light = "15%";
-
-    bg2.classList.add("active");
-bg1.classList.remove("active");
-bg3.classList.remove("active");
-
-changebg();
-})
 
 
 
-bg3.addEventListener("click",()=>{
-    dark = "95%";
-    white = "10%";
-    light = "0%";
-
-    bg3.classList.add("active");
-bg1.classList.remove("active");
-bg2.classList.remove("active");
-
-changebg();
-})
-
-const section = document.querySelectorAll('section[id]');
-window.addEventListener("scroll",highlight);
-
-function highlight(){
-    let scrolly = window.pageYOffset;
-
-    section.forEach(current =>{
-        const sectionheight = current.offsetHeight +9000000;
-        const sectiontop = current.offsetTop -5000 ;
-         const sectionId = current.getAttribute("id");
-
-        if(scrolly>=sectiontop && scrolly<= sectionheight+ sectiontop){
-            document.querySelector(".right a[href*="+ sectionId + ']').classList.add("right-list");
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
         }
-        else{
-            document.querySelector(".right  a[href*="+ sectionId + ']').classList.remove("right-list");
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
-    })
-}
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed1 = new Typed(".typing", {
+        strings: ["Web developer", "Problem Solver"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed2 = new Typed(".typing-2", {
+        strings: ["Web developer", "Problem Solver"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
